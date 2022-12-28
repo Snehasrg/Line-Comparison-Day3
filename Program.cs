32d058029ea54722efe526292620da754e71e261
Snehasrg <sghongade642@gmail.com>
2022-12-28 13:49:43 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Linecomparison
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problem");
+        }
+    }
+}
